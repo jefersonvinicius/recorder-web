8c5627095b068b6ab40b8257689d4fad1195cd8e
Jeferson Vinicius <jeferson.viniciuscrc@gmail.com>
2022-02-16 20:38:25 -0300

    Add audio and video inputs selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { useAudioInputs, useVideosInputs } from './hooks/media-devices';
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
+
+  const [selectedAudioInput, setSelectedAudioInput] = useState<MediaDeviceInfo | null>(null);
+  const [selectedVideoInput, setSelectedVideoInput] = useState<MediaDeviceInfo | null>(null);
+  const [isDisableAudio, setIsDisableAudio] = useState(false);
+  const [isScreenRecording, setIsScreenRecording] = useState(false);
+
+  const { audioInputs } = useAudioInputs();
+  const { videosInputs } = useVideosInputs();
+
+  useEffect(() => {
+    if (!selectedAudioInput && audioInputs.length > 0) {
+      setSelectedAudioInput(audioInputs[0]);
+    }
+
+    if (!selectedVideoInput && videosInputs.length > 0) {
+      setSelectedVideoInput(videosInputs[0]);
+    }
+  }, [audioInputs, selectedAudioInput, selectedVideoInput, videosInputs]);
+
+  function handleAudioInputChange(event: ChangeEvent<HTMLSelectElement>) {
+    const selectedDeviceId = event.target.value;
+    setSelectedAudioInput(audioInputs.find((input) => input.deviceId === selectedDeviceId) ?? null);
+  }
+
+  function handleVideoInputChange(event: ChangeEvent<HTMLSelectElement>) {
+    const selectedDeviceId = event.target.value;
+    setSelectedVideoInput(videosInputs.find((input) => input.deviceId === selectedDeviceId) ?? null);
+  }
+
+  function handleIsDisableAudioChange(event: ChangeEvent<HTMLInputElement>) {
+    setIsDisableAudio(event.target.checked);
+  }
+
+  function handleIsScreenRecordingChange(event: ChangeEvent<HTMLInputElement>) {
+    setIsScreenRecording(event.target.checked);
+  }
+
+  async function handleStartRecordingClick() {
+    const stream = await getStream();
+    videoRef.current!.srcObject = stream;
+
+    async function getStream() {
+      if (isScreenRecording) {
+        return await navigator.mediaDevices.getDisplayMedia({ audio: !isDisableAudio, video: true });
+      }
+      return await navigator.mediaDevices.getUserMedia({
+        video: { deviceId: selectedVideoInput?.deviceId },
+        audio: isDisableAudio ? false : { deviceId: selectedAudioInput?.deviceId },
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="audio-checkbox">Desativar Audio</label>
+      <input type="checkbox" id="audio-checkbox" checked={isDisableAudio} onChange={handleIsDisableAudioChange} />
+      <select value={selectedAudioInput?.deviceId} onChange={handleAudioInputChange} disabled={isDisableAudio}>
+        {audioInputs.map((input) => (
+          <option key={input.deviceId} value={input.deviceId}>
+            {input.label}
+          </option>
+        ))}
+      </select>
+
+      <label htmlFor="record-screen-checkbox">Gravar Própria Tela</label>
+      <input
+        type="checkbox"
+        id="record-screen-checkbox"
+        checked={isScreenRecording}
+        onChange={handleIsScreenRecordingChange}
+      />
+      <select value={selectedVideoInput?.deviceId} onChange={handleVideoInputChange} disabled={isScreenRecording}>
+        {videosInputs.map((input) => (
+          <option key={input.deviceId} value={input.deviceId}>
+            {input.label}
+          </option>
+        ))}
+      </select>
+      <button onClick={handleStartRecordingClick}>Gravar</button>
+      <video ref={videoRef} autoPlay />
     </div>
   );
 }
---
+++ src/hooks/media-devices.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+export function useDevicesSelector(kindToSelect: MediaDeviceKind) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then((devices) => {
+        setDevices(devices.filter((d) => d.kind === kindToSelect));
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [kindToSelect]);
+
+  useEffect(() => {
+    function handleDeviceChange() {
+      navigator.mediaDevices.enumerateDevices().then((devices) => {
+        setDevices(devices.filter((d) => d.kind === kindToSelect));
+      });
+    }
+
+    navigator.mediaDevices.addEventListener('devicechange', handleDeviceChange);
+    return () => navigator.mediaDevices.removeEventListener('devicechange', handleDeviceChange);
+  }, [kindToSelect]);
+
+  return { isLoading, devices };
+}
+
+export function useAudioInputs() {
+  const { devices, ...rest } = useDevicesSelector('audioinput');
+  return { audioInputs: devices, ...rest };
+}
+
+export function useVideosInputs() {
+  const { devices, ...rest } = useDevicesSelector('videoinput');
+  return { videosInputs: devices, ...rest };
+}
