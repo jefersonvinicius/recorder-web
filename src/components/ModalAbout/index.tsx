a3574db8a231a3af9afb13904ce170cfab117cdd
Jeferson Vinicius <jeferson.viniciuscrc@gmail.com>
2022-04-21 12:48:57 -0300

    Fix image path

--- src/components/ModalAbout/index.tsx
+++ src/components/ModalAbout/index.tsx
@@ -37,7 +37,7 @@ export default function ModalAbout({ isOpen, onClose }: Props) {
             <ModalAboutButton onClick={onClose} colorStyle="dark" label="Começar a gravar" labelAlign="center" />
           </ModalAboutContentChild>
           <ModalAboutContentChild>
-            <ModalAboutImg src="%PUBLIC_URL%/imgs/instructions.png" />
+            <ModalAboutImg src={process.env.PUBLIC_URL + '/imgs/instructions.png'} />
           </ModalAboutContentChild>
         </ModalAboutContent>
         <ModalAboutFooter>
